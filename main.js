d5c11eb24cf1006364566cdeda903a44909c18b9
treeebonies <82763578+treeebonies@users.noreply.github.com>
2022-01-10 19:21:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Simar";
+var last_name = "Bhullar";
+var a = 10; 
+var b = 5;
+var add = a + b ;
+console.log(add);
+function concat ()
+{
+    f_name.concat(l_name),
+}
+console.log("Simar");
+console.log("Bhullar");
